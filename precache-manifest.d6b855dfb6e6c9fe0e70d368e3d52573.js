6efb10113697a5b602e89630d77ab85ddf56265f
Nghiem Duong <nghiemduong2000@gmail.com>
2020-08-17 22:21:29 +0700

    Updates

---
+++ precache-manifest.d6b855dfb6e6c9fe0e70d368e3d52573.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6607099023b0141f151f9e004fcd3d",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e87aa64f25fed4688782",
+    "url": "/todolist/static/css/main.0484f81a.chunk.css"
+  },
+  {
+    "revision": "9ed9551ea3246d8e3b2a",
+    "url": "/todolist/static/js/2.9d3904ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist/static/js/2.9d3904ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87aa64f25fed4688782",
+    "url": "/todolist/static/js/main.99debc08.chunk.js"
+  },
+  {
+    "revision": "71e047eee4d7c9bcefd2",
+    "url": "/todolist/static/js/runtime-main.9a44e896.js"
+  },
+  {
+    "revision": "4785ffc8f434dd14867f8326ec680ede",
+    "url": "/todolist/static/media/all.4785ffc8.svg"
+  },
+  {
+    "revision": "d29f1779664178a55ed87236fd97c302",
+    "url": "/todolist/static/media/complete.d29f1779.svg"
+  },
+  {
+    "revision": "40eb9cc6d51d63d1dfd8a0ea29b8d700",
+    "url": "/todolist/static/media/uncomplete.40eb9cc6.svg"
+  }
+]);
